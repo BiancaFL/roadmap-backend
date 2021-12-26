c1aea5020738059e97202500257648c600c967d4
filtre <bianca.filtre.lima@gmail.com>
2021-12-26 16:40:58 -0300

    fixes jsonDB path

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,8 @@
 import { JsonDB } from "node-json-db";
 import { Config } from "node-json-db/dist/lib/JsonDBConfig";
 
-const db = new JsonDB(new Config("roadmapDB", true, false, "/"));
+const db = new JsonDB(
+    new Config(`${process.env.PWD}/src/roadmapDB`, true, false, "/")
+);
 
 export default db;
